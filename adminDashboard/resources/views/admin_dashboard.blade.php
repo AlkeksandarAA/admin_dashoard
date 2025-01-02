b6d8a34361ded452edc6ef664e42cb7682c17cb2
Aleksandar Atanasovski <aleksandaratanasovski43@gmail.com>
2025-01-02 21:01:17 +0100

    adding feat.

---
+++ adminDashboard/resources/views/admin_dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('master.layout')
+
+@section('content')
+
+    <div class="w-1/2 mt-12 ml-10 bg-white flex p-3 rounded-lg shadow-2xl">
+        <form action="">
+            <i class="fa-solid fa-magnifying-glass inline"></i>
+        <input type="text" class="w-3/5  focus:outline-none py-2" placeholder="Search...">
+        </form>
+    </div>
+</div>
+@endsection
---
+++ adminDashboard/resources/views/master/layout.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://kit.fontawesome.com/52dd7732e0.js" crossorigin="anonymous"></script>
+</head>
+<body class="bg-stone-200">
+
+    <div class="grid grid-cols-[auto,1fr] h-screen">
+        <div class="w-60 bg-white p-4">
+            <a href="#">
+                <img src="https://picsum.photos/75/75" alt="random img" class="rounded-full block ml-4 mt-4">
+            </a>
+            <ul class="list-none p-2">
+                <li tabindex="0" class="py-2 px-5 rounded-lg focus:outline-none focus:ring focus:ring-blue-100 hover:bg-blue-100 transition duration-200">
+                    Hello World
+                </li>
+                <li tabindex="0" class="py-2 px-5 rounded-lg focus:outline-none focus:ring focus:ring-blue-100 hover:bg-blue-100 transition duration-200">
+                    Hello World
+                </li>
+                <li tabindex="0" class="py-2 px-5 rounded-lg focus:outline-none focus:ring focus:ring-blue-100 hover:bg-blue-100 transition duration-200">
+                    Hello World
+                </li>
+                <li tabindex="0" class="py-2 px-5 rounded-lg focus:outline-none focus:ring focus:ring-blue-100 hover:bg-blue-100 transition duration-200">
+                    Hello World
+                </li>     <li tabindex="0" class="py-2 px-5 rounded-lg focus:outline-none focus:ring focus:ring-blue-100 hover:bg-blue-100 transition duration-200">
+                    Hello World
+                </li>     <li tabindex="0" class="py-2 px-5 rounded-lg focus:outline-none focus:ring focus:ring-blue-100 hover:bg-blue-100 transition duration-200">
+                    Hello World
+                </li>     <li tabindex="0" class="py-2 px-5 rounded-lg focus:outline-none focus:ring focus:ring-blue-100 hover:bg-blue-100 transition duration-200">
+                    Hello World
+                </li>     <li tabindex="0" class="py-2 px-5 rounded-lg focus:outline-none focus:ring focus:ring-blue-100 hover:bg-blue-100 transition duration-200">
+                    Hello World
+                </li>
+            </ul>
+        </div>
+
+        <div class="p-6">
+            @yield('content')
+        </div>
+    </div>
+  
+</body>
+</html>
